e1ea2a3578bb6937004e218915c5de0fdbc40f24
yousef okasha <36171027+yousefokasha61@users.noreply.github.com>
2022-12-13 16:58:23 +0200

    Update page_56_b.cpp

--- page_56_b.cpp
+++ page_56_b.cpp
@@ -2,20 +2,16 @@
 
 using namespace std;
 
-void print(int arr[], int size, int index, int &sum){
+int sumArray(int arr[], int size, int index){
   if(index == size - 1){
     // cout << arr[index] << " ";
-    sum += arr[index];
-    return;
+    return arr[index];;
   }
   // cout << arr[index] << " ";
-  sum += arr[index];
-  print(arr, size, index+1, sum);
+  return arr[index] + sumArray(arr, size, index + 1);
 }
 
 int main() {
   int arr[] = {1,2,3,4,5,6,7,8};
-  int sum = 0;
-  print(arr,8,0, sum);
-  cout << sum << endl;
+  cout << sumArray(arr,8,0) << endl;
 }
